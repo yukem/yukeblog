5930bac9763adfed83e5e5e355d5c462945ed884
yukem <79051517+yukem@users.noreply.github.com>
2025-06-08 23:50:59 +0900

    Delete wp-content/themes/yukemHP directory

--- wp-content/themes/yukemHP/bat/gulp.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\Users\81904\Local Sites\yukeblogv03\app\public\wp-content\themes\yukemHP
-gulp css-minify&gulp js-minify
-
--- wp-content/themes/yukemHP/content_illustration.php
+++
@@ -1,9 +0,0 @@
-<?php the_post_thumbnail('large', array('class' => 'illustration')); ?>
-
-<?php the_title(); ?>
-<div class="i-content i-hidden">
-  <?php the_title(); ?>
-  <?php the_post_thumbnail('large', array('class' => 'illustration')); ?>
-  <p>ひまわり、ネモフィラ、草、チューリップ、空、雲、すべて手描きでこだわりました。</p>
-  <div class="close">×</div>
-</div>
